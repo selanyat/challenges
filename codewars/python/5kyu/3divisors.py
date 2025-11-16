fda808b280029203adc4db1ce907dde3b4825ec2
selanyat <selanyat@gmail.com>
2025-11-16 23:33:19 +0000

    Add problem to 3divisors

--- codewars/python/5kyu/3divisors.py
+++ codewars/python/5kyu/3divisors.py
@@ -14,3 +14,26 @@ def solution(n, m):
     return result
 
 # print(solution(10000,100000))
+
+
+# Problem
+
+# Description:
+
+# Your task is to write a function that takes two integers n and m, and returns a sorted array of all integers from n to m inclusive, which have exactly 3 divisors (excluding 1 and the number itself).
+# Example:
+
+# solution(2, 20) -> [16]
+
+# 16 has 3 divisors: 2, 4, 8 (1 and 16 aren't included)
+# Input:
+
+    # n - integer (2 ≤ n ≤ 10^14)
+    # m - integer (2 ≤ m ≤ 10^18)
+
+# NOTE: In Rust, the bounds are (2 ≤ n ≤ 2^51), (20 ≤ m ≤ 2^52).
+# Output:
+
+# result - array of integers
+
+
