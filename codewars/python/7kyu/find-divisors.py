ac5d598fd0f7473457db3b5f04387940aa86b698
selanyat <selanyat@gmail.com>
2025-11-17 10:26:06 +0000

    Add input check funtion

--- codewars/python/7kyu/find-divisors.py
+++ codewars/python/7kyu/find-divisors.py
@@ -1,8 +1,19 @@
 #!/bin/env python
 
+def getInput(inp):
+    while not inp.isdigit():
+        print("Input must be a number")
+        inp = input("Enter new digit : ")
+
+    return int(inp)
+
+#while True:
+    #print(getInput())
+
 def divisors(integer):
 
-    integer = int(integer)
+    integer = getInput(integer)
+    print(integer)
     factors = []
 
     for number in range(2, integer):
@@ -15,9 +26,8 @@ def divisors(integer):
     else:
         return f"{integer} is prime"
 
+print(divisors(input("Enter a number: ")))
 
-while True:
-    print(divisors(input("Enter num: ")))
 
 
 # Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#, empty table in COBOL) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
