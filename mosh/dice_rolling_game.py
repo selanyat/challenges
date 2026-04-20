a5130ab584a33c28c565f5f2a939049d5b324f50
Selasi Nyatepe <selanyat@gmail.com>
2026-04-20 08:36:32 +0000

    Implenent optional enhancements

--- mosh/dice_rolling_game.py
+++ mosh/dice_rolling_game.py
@@ -2,29 +2,47 @@
 
 import random
 
-# check if user wants to roll
-# check if option is valid
-# if yes roll dice
-# if no stop game
-
-def getUserInput() -> str:
-    user_input = input("Do you want to roll a dice [y/n]: ").strip().lower()
+def yesOrNo() -> str:
+    user_choice = input("Do you want to roll a dice [y/n]: ").strip().lower()
     # is valid??
-    if user_input not in ["y","n"]:
+    if user_choice not in ["y","n"]:
         print("Invalid Input\nEnter either 'y' or 'n'")
-        return getUserInput()
+        return yesOrNo()
 
-    return user_input
+    return user_choice
 
 def rollDice() -> tuple:
     return (random.randint(1,6), random.randint(1,6))
 
-while True:
-    user_input = getUserInput()
+def setMaxRolls() -> int:
+    user_input = input("Enter maximun number of times to roll dice: ")
 
-    if user_input == "y":
-        print(rollDice())
+    while not user_input.isdigit():
+        print("Invalid Input")
+        return setMaxRolls()
 
-    else:
-        print("Thanks for playing :)")
-        break
+    if int(user_input) < 0:
+        print("Number must be 0 or more")
+        return setMaxRolls()
+
+    return int(user_input)
+
+def main():
+    roll_count = 0
+    max_rolls = setMaxRolls()
+
+    for _ in range(max_rolls):
+        user_choice = yesOrNo()
+
+        if user_choice == "y":
+            roll_count += 1
+            print(rollDice())
+
+        else:
+            print("Thanks for playing :)")
+            break
+
+    print(f"You rolled the dice {roll_count} time(s)")
+
+if __name__ == "__main__":
+    main()
